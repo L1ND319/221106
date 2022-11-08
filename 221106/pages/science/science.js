52b9a62ea3f70636a7c51fa2bdceaa289cabbbfa
L1ND319 <114168732+L1ND319@users.noreply.github.com>
2022-11-08 21:28:05 +0800

    Add files via upload

--- 221106/pages/science/science.js
+++ 221106/pages/science/science.js
@@ -1,11 +1,12 @@
 // pages/science/science.js
+const texts = ['正常月经总量是5mL~80mL，大多数人在20mL~60mL。','红糖水对于缓解痛经的效果主要取决于热水。','月经周期并不都是28天，只要间隔时间稳定就可认为是规律的。','单次月经总量少于5mL则为月经过少，需注意原发病。','单次月经总量多于80mL则为月经过多，需警惕贫血及原发病。','月经流出体外后颜色发黑并且结块是正常的。','痛经分为原发性痛经和继发性痛经，后者需要进一步检查原发病。','原发性痛经的产生跟前列腺素相关，有办法缓解，不用硬扛。','缓解原发性痛经的方法有：非甾体抗炎药、短效避孕药、热量传导。','月经期间体重增加可能跟水钠潴留相关。','使用药物可以推迟月经，但需要至少提前2周遵医嘱用药。']
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
@@ -19,7 +20,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    this.setData({text:texts[0]})
   },
 
   /**
@@ -63,22 +64,11 @@ Page({
   onShareAppMessage() {
 
   },
-  nametxt: document.getElementById('name'),
-  button: document.getElementById('button_text'),
-        // 创建一个数组存储名字
-        uname: ['阿阳热爱前端','郝嘉慧','冬灰条','蒸羊羔儿','蒸熊掌','蒸鹿尾儿',
-				'烧花鸭','烧雏鸡','烧子鹅','炉猪','炉鸭','酱鸡','腊肉','松花',
-				'小肚儿','晾肉','香肠儿','什锦苏盘儿','熏鸡白肚儿','清蒸八宝猪',
-				'江米酿鸭子','罐儿野鸡','罐儿鹌鹑','卤什件儿','卤子鹅','山鸡','兔脯',
-				'菜蟒','银鱼','清蒸哈什蚂','烩鸭腰儿','鸭条','清拌腰丝儿','黄心管儿'],
-        // 创建一个函数生成随机数字
-        getrandom:function (min,max){
-            return Math.floor(Math.random()*(max-min-1)+min);
-        },
-        clock: function (){
-            // 通过获取一个随机的数组下标实现随机获取一个名字，并将这个名字赋值给变量random
-            var random = uname[getrandom(0,uname.length-1)];
-            //将random塞到span里
-            nametxt.innerHTML=random;
-        },
+  flash: function(){
+    function getrandom(min,max){
+      return Math.floor(Math.random()*(max-min-1)+min);
+  }
+  this.setData({text:texts[getrandom(0,texts.length-1)]})
+  }
+  
 })
